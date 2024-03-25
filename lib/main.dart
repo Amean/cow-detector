9588c4a6b5015e7bc88d1fa9fc7878188d0f8f9b
Christine Amin <christine.t.amin@gmail.com>
2024-03-25 16:23:43 -0500

    set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cow_detector/screens/home_screen.dart';
 import 'package:cow_detector/screens/login_screen.dart';
 import 'package:cow_detector/screens/settings_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+// Ideal time to initialize
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+//...
+
   runApp(const CowDetectorApp());
 }
 
