39c5795f7c38f3ddc9e65e3e04c211c27a86ad4e
Christine Amin <christine.t.amin@gmail.com>
2024-03-27 12:31:15 -0500

    added statements and widgets to switch between editing and viewing emai and password

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:cow_detector/services/auth_service.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,9 +10,23 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final _auth = AuthService();
+  bool isEditingEmail = false;
+  bool isEditingPassword = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () {
+            context.go('/home');
+          },
+        ),
+      ),
       body: Container(
         constraints: const BoxConstraints(minWidth: 300, maxWidth: 500),
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
@@ -18,15 +34,81 @@ class _SettingsScreenState extends State<SettingsScreen> {
           children: [
             const Icon(Icons.settings),
             Text('Email'),
-            Text('Passoword'),
+            if (!isEditingEmail)
+              Row(
+                children: [
+                  Text('email'), //_auth.getCurrentUser().email),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.edit,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isEditingEmail = true;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if (isEditingEmail)
+              Row(
+                children: [
+                  TextFormField(),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.save,
+                    ),
+                    onPressed: () {
+                      // _auth.updateEmail(newEmail);
+                      setState(() {
+                        isEditingEmail = false;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            Text('Password'),
+            if (!isEditingPassword)
+              Row(
+                children: [
+                  Text('password'), //_auth.getCurrentUser().email),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.edit,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isEditingPassword = true;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if (isEditingPassword)
+              Row(
+                children: [
+                  TextFormField(),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.save,
+                    ),
+                    onPressed: () {
+                      // _auth.updateEmail(newEmail);
+                      setState(() {
+                        isEditingPassword = false;
+                      });
+                    },
+                  ),
+                ],
+              ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => {},
+              onPressed: () => _auth.signOut(),
               child: const Text('Log Out'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => {},
+              onPressed: () => _auth.deleteAccount(),
               child: const Text('Delete Account'),
             ),
           ],
